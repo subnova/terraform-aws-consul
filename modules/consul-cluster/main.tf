86ccee12feb720549a07d6f1d4a9d1426415123a
Dale Peakall <dale.peakall.1@lexisnexis.co.uk>
2018-06-04 13:10:17 +0000

    Allowed max_capacity to exceed desired_capacity to support rolling update.

--- modules/consul-cluster/main.tf
+++ modules/consul-cluster/main.tf
@@ -22,7 +22,7 @@ resource "aws_autoscaling_group" "autoscaling_group" {
 
   # Run a fixed number of instances in the ASG
   min_size             = "${var.cluster_size}"
-  max_size             = "${var.cluster_size}"
+  max_size             = "${var.cluster_size + 1}"
   desired_capacity     = "${var.cluster_size}"
   termination_policies = ["${var.termination_policies}"]
 
