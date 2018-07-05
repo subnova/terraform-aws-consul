a646756304f18c102f7b906f2cd69ce042f2da25
Dale Peakall <dale.peakall.1@lexisnexis.co.uk>
2018-07-05 15:05:42 +0000

    Set max-size of ASG to double the desired size to allow for replacement.

--- modules/consul-cluster/main.tf
+++ modules/consul-cluster/main.tf
@@ -20,7 +20,7 @@ resource "aws_autoscaling_group" "autoscaling_group" {
 
   # Run a fixed number of instances in the ASG
   min_size             = "${var.cluster_size}"
-  max_size             = "${var.cluster_size + 1}"
+  max_size             = "${var.cluster_size * 2}"
   desired_capacity     = "${var.cluster_size}"
   termination_policies = ["${var.termination_policies}"]
 
